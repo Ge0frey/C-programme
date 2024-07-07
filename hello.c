eab8d01f26727d52feb13235946808418078a092
Ge0frey <jeffkimzyjoe@gmail.com>
2024-07-08 02:01:09 +0300

    Using two %s

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void) {
-    string answer = get_string ("What is your name? ");
-    printf("Hello, %s \n", answer);
+int main (void) {
+    string first = get_string("What is your first name?\n");
+    string last = get_string("What is your last name?\n");
+    printf("My name is %s %s \n", first, last);
 }

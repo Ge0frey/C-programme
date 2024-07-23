82fec3ece1cdd3d6bd2492866631a76e347147f9
Ge0frey <jeffkimzyjoe@gmail.com>
2024-07-23 20:18:15 +0300

    Strings as chars,arrays and integers

---
+++ chars.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main () {
+    string greeting = "Hello";
+    printf("%i%i%i%i%i%i\n", greeting[0],greeting[1],greeting[2],greeting[3],greeting[4],greeting[5]);
+}

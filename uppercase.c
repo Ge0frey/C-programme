d98d1d8168287892f00b5f1714d11829af5981f7
Ge0frey <jeffkimzyjoe@gmail.com>
2024-07-25 01:05:08 +0300

    Changing into uppercase

---
+++ uppercase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+int main (void) {
+    string uppercase = get_string("Before: ");
+    printf("After:  ");
+    for (int i = 0; i < strlen(uppercase); i++) {
+        if (uppercase[i] >= 'a' && uppercase[i] <= 'z')
+        {
+            printf("%c", uppercase[i] - 32);
+        } else {
+            printf("%c", uppercase[i]);
+        }
+    }
+    printf("\n");
+}

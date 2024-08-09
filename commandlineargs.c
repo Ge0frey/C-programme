b4b469325906e6717ad72f23ab1853f81303fa0f
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-09 23:31:01 +0300

    more on how to use command line arguments

--- commandlineargs.c
+++ commandlineargs.c
@@ -5,11 +5,9 @@
 //when the parameter at int main is void it means the programme doesn't allow the use of commandline arguments;
 //However, when the function main takes parameters argc and argv it means that the programme can take commandline arguments;
 
-int main(int argc, string argv[])
-{
-    if (argc == 2) {
-        printf("Hello %s\n", argv[1]);
-    } else {
-        printf("Hello world\n");
+int main (int argc, string argv[]) {
+    if (argc == 3) {
+        printf("My name is %s %s\n", argv[2], argv[1]);
     }
+    return 0;
 }

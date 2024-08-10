d3b7262be6eb0837c272a359dc7ce2e96a4f2853
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-10 17:23:21 +0300

    linear search on strings using strcmp

--- searchalgo.c
+++ searchalgo.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
-    int numbers[] = {20,500,10,5,100,1,50};
-    
-    int n = get_int("Enter number: \n");
+    string strings[] = {"Tuahajok", "Guvocriw", "Luevwu", "Cekiepi", "Kicedahi", "Umnicpe", "Datdike"};
 
-    for (int i = 0; i < 7; i++ ) {
-        if (numbers[i] == n) {
-            printf("Number found\n");
+    string s = get_string("Enter city name: \n");
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (strcmp(strings[i], s) == 0)
+        {
+            printf("City found\n");
             return 0;
         }
     }
-    printf("Number not found\n");
+    printf("City not found\n");
     return 1;
 }

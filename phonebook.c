b381918c3d5331b2a09c26ff6100604bc33f73ba
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-11 02:18:48 +0300

    Creates custom data structures

---
+++ phonebook.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+
+typedef struct 
+{
+    string names;
+    string numbers;
+}
+person;
+
+int main(void)
+{
+    person people[2];
+
+    people[0].names = "Ricky Leonard";
+    people[0].numbers = "(806) 395-3944";
+
+    people[1].names = "Delia Craig";
+    people[1].numbers = "(980) 247-6323";
+
+    string contact = get_string("Enter name: ");
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(people[i].names, contact) == 0)
+        {
+            printf("the contact is %s\n", people[i].numbers);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}

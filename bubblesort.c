17b8067d9db2b325b2dca192df0510b04215a5fc
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-14 23:54:28 +0300

    code refactor

--- bubblesort.c
+++ bubblesort.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 
-int main () {
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        // Last i elements are already sorted
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                // Swap arr[j] and arr[j+1]
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 
-    int arr[8] = {3, 7, 2, 8, 5, 1, 4, 6};
-
+int main() {
+    int arr[8] = {3, 7, 2, 8, 5, 1, 4, 6};  // Example array with random numbers
     int n = 8;
-
-    bubblesort(arr, n);
-
+    
+    bubbleSort(arr, n);
+    
     printf("Sorted array: ");
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
-
+    
     return 0;
-}
-
-void bubblesort (int arr[], int n) {
-    int i;
-    int j;
-    int temp;
-
-    for (i = 0; i < n-1; i++) {
-        for (j = 0; j < n-1-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                temp = arr[j];
-                arr[j+1] = temp;
-            }
-        }
-    };
 }

c9f01868b00c7949866969eb71689af4aa5eb1aa
Ge0frey <jeffkimzyjoe@gmail.com>
2024-08-15 22:16:07 +0300

    C iteration

---
+++ iteration.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+void draw (int n);  //make sure to always include the prototype
+
+int main (void) {
+    int brick = get_int("Enter number of bricks: ");
+
+    draw(brick);
+}
+
+void draw (int n) {
+    for(int i = 0; i < n; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            printf("#");
+        }
+        printf ("\n"); 
+    }
+}

0d76e076263db1127d013f9d542f44f2a017eb32
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-01 21:11:23 +0300

    chore(format):format syntax for enhaced code readability

--- bubblesort.c
+++ bubblesort.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int arr[], int n)
+{
     int i, j, temp;
-    for (i = 0; i < n-1; i++) {
+    for (i = 0; i < n - 1; i++)
+    {
         // Last i elements are already sorted
-        for (j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 // Swap arr[j] and arr[j+1]
                 temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
     }
 }
 
-int main() {
-    int arr[8] = {3, 7, 2, 8, 5, 1, 4, 6};  // Example array with random numbers
+int main()
+{
+    int arr[8] = {3, 7, 2, 8, 5, 1, 4, 6}; // Example array with random numbers
     int n = 8;
-    
+
     bubbleSort(arr, n);
-    
+
     printf("Sorted array: ");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         printf("%d ", arr[i]);
     }
-    
+
     return 0;
 }

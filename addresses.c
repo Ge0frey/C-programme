5d7dbbc60597a39601b87e9bed2e7fea1e8ac3cb
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-06 00:09:13 +0300

    create the string type

--- addresses.c
+++ addresses.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <cs50.h>
-
 
 int main () 
 {
+    typedef char * string; //I have created a datatype called string which essentially is char *
     char * s = "Hi"; //A string is basically a pointer to a character
     string * ps = &s; 
 
-    printf("String pointer: %p\n", s);  //Will print the string pointer
+    printf("String pointer: %s\n", s);  //Will print the string pointer
     printf ("First character pointer: %p\n", ps[0]);  //Will print the first character of our string
     return 0; //Will terminate our operation
 }

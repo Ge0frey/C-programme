22e680ff597e9efdc484c71b25f17be7358146fc
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-05 07:19:20 +0300

    pointer to individual elements of a string

--- addresses.c
+++ addresses.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <cs50.h>
+
+string s = "Hi";
+
+string * ps = &s; 
+
 
 int main () 
 {
     int n = 60;
     int * p = &n;
-    printf("%p\n", p);
+    printf("pointer of n variable: %p\n", p);
+    printf ("String pointer: %p\n%p\n", ps, ps[0]);
     return 0;
 }

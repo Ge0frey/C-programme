eefe8bf395192cbd65abdf758000c0e15f9d708a
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-06 07:17:06 +0300

    print characters each in line

--- addresses.c
+++ addresses.c
@@ -6,9 +6,9 @@ int main()
     string s = "Hi!";       // A string is basically a pointer to a character
 
     printf("String pointer: %p\n", s);              // Will print the string pointer
-    printf("First character pointer: %p\n", &s[0]); // Will print the first character of our string
-    printf("Second character pointer: %p\n", &s[1]); // Will print the second character of our string
-    printf("Third character pointer: %p\n", &s[2]); // Will print the third character of our string
+    printf("First character pointer: %c\n", s[0]); // Will print the first character of our string
+    printf("Second character pointer: %c\n", s[1]); // Will print the second character of our string
+    printf("Third character pointer: %c\n", s[2]); // Will print the third character of our string
     printf("Null character pointer: %p\n", &s[3]); // Will print the null escape pointer of our string
     return 0;                                       // Will terminate our operation
 }

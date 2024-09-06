1dba96d37ef39936699e6b43c3da40562a1272d3
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-06 07:28:11 +0300

    segmentation fault(Referencing something that is out of memory)

--- addresses.c
+++ addresses.c
@@ -7,6 +7,6 @@ int main()
 
     printf("First character pointer: %c\n", *s); // Will print the first character of our string
     printf("Second character pointer: %c\n", *(s+1)); // Will print the second character of our string
-    printf("Third character pointer: %c \n", *(s+2)); // Will print the third character of our string
+    printf("Third character pointer: %c \n", *(s+50000)); // Will print the third character of our string
     return 0;                                       // Will terminate our operation
 }

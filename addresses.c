43cbc39192a6303f9c88be210c84c618247f201d
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-06 08:00:11 +0300

    pointer arithmetics

--- addresses.c
+++ addresses.c
@@ -5,8 +5,8 @@ int main()
     typedef char *string; // I have created a datatype called string which essentially is char *
     string s = "Hi!";       // A string is basically a pointer to a character
 
-    printf("First character pointer: %c\n", *s); // Will print the first character of our string
-    printf("Second character pointer: %c\n", *(s+1)); // Will print the second character of our string
-    printf("Third character pointer: %c \n", *(s+50000)); // Will print the third character of our string
+    printf("First character pointer: %s\n", s); // Will print the first character of our string
+    printf("Second character pointer: %s\n", (s+1)); // Will print the second character of our string
+    printf("Third character pointer: %s\n", (s+2)); // Will print the third character of our string
     return 0;                                       // Will terminate our operation
 }

346be0e45cc262da7926686353bc9b0d9f32a9d0
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-07 18:48:32 +0300

    Ensuring rhere is always a string input to avoid segmentation fault

--- copy.c
+++ copy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 #include <ctype.h>
 
 int main () {
@@ -9,7 +10,9 @@ int main () {
     string s = get_string("Enter string: ");
     string t = s;
 
-    t[0] = toupper(t[0]);
+    if(strlen(t) > 0) {
+        t[0] = toupper(t[0]);
+    }
 
     printf("%p\n", s);
     printf("%p\n", t);

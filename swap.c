c407a2c2f6a375915c5e79427ee83a29bb5f4ae9
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-10 01:27:05 +0300

    Swapping based off addresses and references over values

--- swap.c
+++ swap.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int a, int b)
+void swap(int *a, int *b) // swap function taking two pointer integers as parameters
 {
-    int tmp = a;
-    a = b;
-    b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 int main(void)
 {
-    int x = 1;
-    int y = 2;
+    int x = 1; // initializing the vaue of x
+    int y = 2; // initializing the vaue of y
 
-    printf("x is %i, y is %i\n", x, y);
-    swap(x,y);
-    printf("x is %i, y is %i\n", x, y);
+    printf("x is %i, y is %i\n", x, y); // printing before swap
+    swap(&x, &y);                         // Calling the swap function passing x and y as arguments to the function
+    printf("x is %i, y is %i\n", x, y); // printing after swap
 }

8c1aa34922464581ee32baf8fdbbf9c5b2da19c5
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-10 02:13:22 +0300

    Getting user inputs for strings

--- get.c
+++ get.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main (void) 
+int main(void)
 {
     int x;
     int y;
+    char s[255];
 
     printf("Enter x; ");
-    scanf("%i", &x); //storing by reference and not by value
-    printf("Enter y; "); 
-    scanf("%i", &y); //Do not use the newline character \n in scanf as it is treated like white space
+    scanf("%i", &x); // storing by reference and not by value
+    printf("Enter y; ");
+    scanf("%i", &y); // Do not use the newline character \n in scanf as it is treated like white space
+    getchar(); //clears the new line character left behind by scanf
+    printf("Enter string: ");
+    fgets(s, 255, stdin);
 
-    printf("The value of x is %i and the value of y is %i\n", x,y);
+    printf("The value of x is %i and the value of y is %i when the string is %s", x, y, s);
 }

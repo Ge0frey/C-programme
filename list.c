715b1e7d2c6385e60cdd64b26ed50715a3d46f09
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-15 01:58:47 +0300

    Adding to an array using malloc

--- list.c
+++ list.c
@@ -5,7 +5,7 @@ int main()
 {
     int *list = malloc(3 * sizeof(int));
 
-    if (list == NULL) 
+    if (list == NULL)
     {
         return 1;
     }
@@ -14,9 +14,29 @@ int main()
     list[1] = 2;
     list[2] = 3;
 
-    int i;
-    for (i = 0; i < sizeof(int); i++)
+    int *tmp = malloc(4 * sizeof(int));
+
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    for (int i = 0; i < 3; i++)
     {
         printf("%i\n", list[i]);
     }
+
+    free(list);
+    return 0;
 }

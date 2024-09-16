8755975783f4768dd2ed4ae2a7e64b34539307ed
Ge0frey <jeffkimzyjoe@gmail.com>
2024-09-16 22:23:34 +0300

    mario bricks in C using nested loops

--- marioexercise.c
+++ marioexercise.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
-
-int get_size(void);
-void print_brick(int size);
+#include <cs50.h>
 
 int main(void)
 {
-    int n = get_size();
-    print_brick(n);
-}
-
-int get_size(void)
-{
-    int n;
+    int height;
     do
     {
-        n = get_int("What is the size of the brick? ");
-    } while (n < 1);
-    return n;
-}
-
-void print_brick(int size)
-{
-    for (int i = 0; i < size; i++)
+        height = get_int("Enter height of brick: ");
+    } while (height < 1);
+    for (int row = 0; row < height; row++)
     {
-        for (int j = 0; j < size; j++)
+        printf("%i", row);
+        for (int column = 0; column < height; column++)
         {
-            printf("?");
+            printf("%i", column); 
         }
         printf("\n");
     }
